932d259128df5929bc537a7cc7e4e9599ee9df79
Robert Queeney <robert.queeney@gmail.com>
2021-04-18 17:24:57 -0500

    front and back end added

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express'); 
+// const bodyParser = require("body-parser");
+// const cors = require('cors');
+
+const fetch = require('node-fetch');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const apiUrl = 'https://gis.hennepin.us/arcgis/rest/services/HennepinData/PLACES/MapServer/8/query?outFields=*&where=1%3D1'
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => console.log(data))
+})
+
+app.listen(3001, function () {
+    console.log('Example app listening on port 3001.');
+});
