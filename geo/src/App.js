019f911ac39b1a19c9c8695d0cb94b693371b18b
Robert Queeney <robert.queeney@gmail.com>
2021-04-24 14:35:59 -0500

    fetch call working cannot map over results

--- geo/src/App.js
+++ geo/src/App.js
@@ -1,26 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-import React,  { Component, Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 class App extends Component {
-  
+
+  state = {
+    crimes: ""
+  }
+
+  async componentDidMount() {
+    const url = 'https://services.arcgis.com/afSMGVsC7QlRK1kZ/arcgis/rest/services/PoliceOffense2016/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+    const response = await fetch(url)
+    const data = await response.json()
+    this.setState({ crimes: data.features })
+    console.log(this.state.crimes[0])
+    // console.log(this.state.crimes.features[0].geometry.x)
+  }
+
   render() {
-   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-    <TileLayer
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
- 
-   );
+    return (
+      <MapContainer center={[44.986656, -93.258133]} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* Need to map over crimes and put points on the map */}
+          {/* {this.state.crimes.map(crime => { */}
+            <Marker position={[44.986656, -93.258133]}>
+            
+            {/* <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup> */}
+          </Marker>
+           {/* })} */}
+          
+
+        
+
+      </MapContainer>
+
+
+
+    );
   }
-  }
+}
 
 export default App;
